3ee7805a95852050f5ac677a257c2827ef5800d1
Ricardo Mercado <ricardoamercado@gmail.com>
2017-10-17 16:29:24 -0500

    [FEATURE] Add atm spots data. Create atm data seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,32 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias AiudaprApi.Repo
+alias AiudaprApi.Spot
+
+defmodule AtmData do
+  def import do
+    Path.expand("../data/atm_data.json", __DIR__)
+    |> File.read!
+    |> Poison.decode!
+    |> do_create_spots
+  end
+
+  defp do_create_spots(data) do
+    Enum.each data["features"], fn(spot) ->
+      do_create_spot(spot)
+    end
+  end
+
+  defp do_create_spot(spot) do
+    Repo.insert! %Spot{
+      lat: Enum.at(spot["geometry"]["coordinates"], 1),
+      lon: Enum.at(spot["geometry"]["coordinates"], 0),
+      kind: "atm",
+      details: spot["properties"]["bank"]
+    }
+  end
+end
+
+AtmData.import
